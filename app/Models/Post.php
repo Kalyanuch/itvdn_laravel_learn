fdcb5ce74a256a5e53ec1f645fd7418e699aa301
Shamrylo Mykola <itlutsk@gmail.com>
2022-03-27 14:59:20 +0300

    Make changes for Post model 2022-03-27

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    const CREATED_AT = 'createdAt';
+    const UPDATED_AT = 'updatedAt';
+
+    public $table = 'posts';
+
+    protected $fillable = [
+        'title', 'body', 'userId'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
 }
