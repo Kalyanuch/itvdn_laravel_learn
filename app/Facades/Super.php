cfe8f8a81661d577e8bccb725529616e33cd1f4b
Shamrylo Mykola <itlutsk@gmail.com>
2022-03-28 17:47:31 +0300

    Get component using facade 2022-03-28

---
+++ app/Facades/Super.php
@@ -0,0 +1,13 @@
+<?php
+ namespace App\Facades;
+
+ use App\Components\SuperComponent;
+ use Illuminate\Support\Facades\Facade;
+
+ class Super extends Facade
+ {
+     protected static function getFacadeAccessor()
+     {
+         return SuperComponent::class;
+     }
+ }
--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Components\SuperComponent;
-use Illuminate\Http\Request;
+//use App\Components\SuperComponent;
+use App\Facades\Super;
 
 class TestController extends Controller
 {
-    public function testProvider(SuperComponent $component)
+    //public function testProvider(SuperComponent $component)
+    public function testProvider()
     {
+        /*
         return view('provider', ['name' => $component->getName(), 'prop' => $component->getProperty()]);
+        */
+        return view('provider', ['name' => Super::getName(), 'prop' => Super::getProperty()]);
     }
 }
